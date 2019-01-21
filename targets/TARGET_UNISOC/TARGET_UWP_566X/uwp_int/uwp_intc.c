c723e1be6edaf0b6397b9833c96107d2aa4e4184
xiangkai.gao <xiangkai.gao@unisoc.com>
2019-01-21 19:24:15 +0800

    modify intc

--- targets/TARGET_UNISOC/TARGET_UWP_566X/uwp_int/uwp_intc.c
+++ targets/TARGET_UNISOC/TARGET_UWP_566X/uwp_int/uwp_intc.c
@@ -4,161 +4,173 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
-#include "mbed_retarget.h"
 #include "uwp_intc.h"
-#include "UWP_5661.h"
+#include "hal_intc.h"
 #include "uwp_log.h"
-
 
 /* convenience defines */
 struct __intc_cb {
-	uwp_intc_callback_t cb;
-	void *data;
+    uwp_intc_callback_t cb;
+    void *data;
 };
 
 struct intc_uwp_data {
-	struct __intc_cb irq[INTC_MAX_CHANNEL];
-	struct __intc_cb fiq[INTC_MAX_CHANNEL];
+    struct __intc_cb irq[INTC_MAX_CHANNEL];
+    struct __intc_cb fiq[INTC_MAX_CHANNEL];
 };
 
 static struct intc_uwp_data intc_uwp_dev_data;
 static struct intc_uwp_data aon_intc_uwp_dev_data;
 
 void uwp_intc_set_irq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg)
+        uwp_intc_callback_t cb, void *arg)
 {
-	if (intc_uwp_dev_data.irq[channel].cb != NULL) {
+    if (intc_uwp_dev_data.irq[channel].cb != NULL) {
         LOG_WRN("IRQ %d callback has been registered.\n", channel);
         return;
-	}
+    }
 
-	intc_uwp_dev_data.irq[channel].cb = cb;
-	intc_uwp_dev_data.irq[channel].data = arg;
+    intc_uwp_dev_data.irq[channel].cb = cb;
+    intc_uwp_dev_data.irq[channel].data = arg;
 }
 
 void uwp_intc_unset_irq_callback(int channel)
 {
-	intc_uwp_dev_data.irq[channel].cb = NULL;
-	intc_uwp_dev_data.irq[channel].data = NULL;
+    intc_uwp_dev_data.irq[channel].cb = NULL;
+    intc_uwp_dev_data.irq[channel].data = NULL;
 }
 
 void uwp_intc_set_fiq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg)
+        uwp_intc_callback_t cb, void *arg)
 {
-	if (intc_uwp_dev_data.fiq[channel].cb != NULL) {
+    if (intc_uwp_dev_data.fiq[channel].cb != NULL) {
         LOG_WRN("FIQ %d callback has been registered.\n", channel);
         return;
-	}
+    }
 
-	intc_uwp_dev_data.fiq[channel].cb = cb;
-	intc_uwp_dev_data.fiq[channel].data = arg;
+    intc_uwp_dev_data.fiq[channel].cb = cb;
+    intc_uwp_dev_data.fiq[channel].data = arg;
 }
 
 void uwp_intc_unset_fiq_callback(int channel)
 {
-	intc_uwp_dev_data.fiq[channel].cb = NULL;
-	intc_uwp_dev_data.fiq[channel].data = NULL;
+    intc_uwp_dev_data.fiq[channel].cb = NULL;
+    intc_uwp_dev_data.fiq[channel].data = NULL;
 }
 
 void uwp_aon_intc_set_irq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg)
+        uwp_intc_callback_t cb, void *arg)
 {
 
-	if (aon_intc_uwp_dev_data.irq[channel].cb != NULL) {
+    if (aon_intc_uwp_dev_data.irq[channel].cb != NULL) {
         LOG_WRN("IRQ %d callback has been registered.\n", channel);
         return;
-	}
+    }
 
-	aon_intc_uwp_dev_data.irq[channel].cb = cb;
-	aon_intc_uwp_dev_data.irq[channel].data = arg;
+    aon_intc_uwp_dev_data.irq[channel].cb = cb;
+    aon_intc_uwp_dev_data.irq[channel].data = arg;
 }
 
 void uwp_aon_intc_unset_irq_callback(int channel)
 {
 
-	aon_intc_uwp_dev_data.irq[channel].cb = NULL;
-	aon_intc_uwp_dev_data.irq[channel].data = NULL;
+    aon_intc_uwp_dev_data.irq[channel].cb = NULL;
+    aon_intc_uwp_dev_data.irq[channel].data = NULL;
 }
 
-#if 0
-DEVICE_INIT(intc_uwp, CONFIG_INTC_UWP_DEVICE_NAME,
-		    intc_uwp_init, &intc_uwp_dev_data, NULL,
-		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+void uwp_ictl_irq_enable(uwp_irq_type_t irq_type, unsigned int irq){
+    volatile struct uwp_intc *intc = NULL;
+    if(irq_type == AON_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)BASE_AON_INTC;
+    else if(irq_type == IRQ_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)BASE_INTC;
+    else if(irq_type == FIQ_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)(BASE_INTC + 0x20);
+    uwp_intc_enable(intc, irq);
+}
 
-DEVICE_INIT(aon_intc_uwp, CONFIG_AON_INTC_UWP_DEVICE_NAME,
-		    aon_intc_uwp_init, &aon_intc_uwp_dev_data, NULL,
-		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
-#endif
+void uwp_ictl_irq_disable(uwp_irq_type_t irq_type, unsigned int irq){
+    volatile struct uwp_intc *intc = NULL;
+    if(irq_type == AON_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)BASE_AON_INTC;
+    else if(irq_type == IRQ_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)BASE_INTC;
+    else if(irq_type == FIQ_IRQ_TYPE)
+        intc = (volatile struct uwp_intc *)(BASE_INTC + 0x20);
+    uwp_intc_disable(intc, irq);
+}
 
 static void intc_uwp_irq(void)
 {
-	int ch;
-	for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
-		if (uwp_irq_status(ch) && intc_uwp_dev_data.irq[ch].cb) {
-			uwp_irq_disable(ch);
-			intc_uwp_dev_data.irq[ch].cb(ch, intc_uwp_dev_data.irq[ch].data);
-			uwp_irq_enable(ch);
-		}
-	}
+    mbed_error_printf("%s\r\n",__func__);
+    volatile struct uwp_intc * intc = (volatile struct uwp_intc *)BASE_INTC;
+    u32_t status = uwp_intc_status(intc);
+    int ch;
+    for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
+        if ((status & (0x1UL<<ch)) && intc_uwp_dev_data.irq[ch].cb) {
+            uwp_intc_disable(intc, ch);
+            intc_uwp_dev_data.irq[ch].cb(ch, intc_uwp_dev_data.irq[ch].data);
+            uwp_intc_enable(intc, ch);
+        }
+    }
 }
 
 static void intc_uwp_fiq(void)
 {
-	int ch;
-	for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
-		if (uwp_fiq_status(ch) && intc_uwp_dev_data.fiq[ch].cb) {
-			uwp_fiq_disable(ch);
-			intc_uwp_dev_data.fiq[ch].cb(ch, intc_uwp_dev_data.fiq[ch].data);
-			uwp_fiq_enable(ch);
-		}
-	}
+    mbed_error_printf("%s\r\n",__func__);
+    volatile struct uwp_intc * intc = (volatile struct uwp_intc *)(BASE_INTC + 0x20);
+    u32_t status = uwp_intc_status(intc);
+    int ch;
+    for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
+        if ((status & (0x1UL<<ch)) && intc_uwp_dev_data.fiq[ch].cb) {
+            uwp_intc_disable(intc, ch);
+            intc_uwp_dev_data.fiq[ch].cb(ch, intc_uwp_dev_data.fiq[ch].data);
+            uwp_intc_enable(intc, ch);
+        }
+    }
 }
-
 
 static void aon_intc_uwp_irq(void)
 {
-	int ch;
-	for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
-		if (uwp_aon_irq_status(ch) && aon_intc_uwp_dev_data.irq[ch].cb) {
-			uwp_aon_irq_disable(ch);
-			aon_intc_uwp_dev_data.irq[ch].cb(ch, aon_intc_uwp_dev_data.irq[ch].data);
-			uwp_aon_irq_enable(ch);
-		}
-	}
+    volatile struct uwp_intc * intc = (volatile struct uwp_intc *)BASE_AON_INTC;
+    u32_t status = uwp_intc_status(intc);
+    int ch;
+    for (ch = 0; ch < INTC_MAX_CHANNEL; ch++) {
+        if ((status & (0x1UL<<ch)) && aon_intc_uwp_dev_data.irq[ch].cb) {
+            uwp_intc_disable(intc, ch);
+            LOG_DBG("AON ch:%d\r\n",ch);
+            aon_intc_uwp_dev_data.irq[ch].cb(ch, aon_intc_uwp_dev_data.irq[ch].data);
+            uwp_intc_enable(intc, ch);
+        }
+    }
 }
 
 void intc_uwp_init(void)
 {
     NVIC_DisableIRQ(INT_IRQ_IRQn);
-	NVIC_DisableIRQ(INT_FIQ_IRQn);
+    NVIC_DisableIRQ(INT_FIQ_IRQn);
 
-	uwp_sys_enable(BIT(APB_EB_INTC));
-	uwp_sys_reset(BIT(APB_EB_INTC));
+    uwp_sys_enable(BIT(APB_EB_INTC));
+    uwp_sys_reset(BIT(APB_EB_INTC));
 
-    NVIC_SetVector(INT_IRQ_IRQn,intc_uwp_irq);
-	NVIC_SetPriority(INT_IRQ_IRQn,0x1FUL);
-	NVIC_EnableIRQ(INT_IRQ_IRQn);
+    NVIC_SetVector(INT_IRQ_IRQn,(uint32_t)intc_uwp_irq);
+    NVIC_SetPriority(INT_IRQ_IRQn,0x1FUL);
+    NVIC_EnableIRQ(INT_IRQ_IRQn);
 
-    NVIC_SetVector(INT_FIQ_IRQn, intc_uwp_fiq);
-	NVIC_SetPriority(INT_FIQ_IRQn,0x1FUL);
-	NVIC_EnableIRQ(INT_FIQ_IRQn);
-
+    NVIC_SetVector(INT_FIQ_IRQn,(uint32_t)intc_uwp_fiq);
+    NVIC_SetPriority(INT_FIQ_IRQn,0x1FUL);
+    NVIC_EnableIRQ(INT_FIQ_IRQn);
 }
-
 
 void aon_intc_uwp_init(void)
 {
     NVIC_DisableIRQ(AON_IRQn);
 
-	uwp_aon_enable(BIT(AON_EB_INTC));
-	uwp_aon_reset(BIT(AON_RST_INTC));
+    uwp_aon_enable(BIT(AON_EB_INTC));
+    uwp_aon_reset(BIT(AON_RST_INTC));
     
-	NVIC_SetVector(AON_IRQn,aon_intc_uwp_irq);
-	NVIC_SetPriority(AON_IRQn,0x1FUL);
-	NVIC_EnableIRQ(AON_IRQn);
-	
+    NVIC_SetVector(AON_IRQn,(uint32_t)aon_intc_uwp_irq);
+    NVIC_SetPriority(AON_IRQn,0x1FUL);
+    NVIC_EnableIRQ(AON_IRQn);
 }
-
-
 
--- targets/TARGET_UNISOC/TARGET_UWP_566X/uwp_int/uwp_intc.h
+++ targets/TARGET_UNISOC/TARGET_UWP_566X/uwp_int/uwp_intc.h
@@ -7,16 +7,26 @@
 #ifndef MBED_UWP_INTC_H
 #define MBED_UWP_INTC_H
 
+typedef enum{
+    FIQ_IRQ_TYPE,
+    IRQ_IRQ_TYPE,
+    AON_IRQ_TYPE
+}uwp_irq_type_t;
+
 typedef void (*uwp_intc_callback_t) (int channel, void *user);
 
 extern void uwp_intc_set_irq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg);
+        uwp_intc_callback_t cb, void *arg);
 extern void uwp_intc_unset_irq_callback(int channel);
 extern void uwp_intc_set_fiq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg);
+        uwp_intc_callback_t cb, void *arg);
 extern void uwp_intc_unset_fiq_callback(int channel);
 extern void uwp_aon_intc_set_irq_callback(int channel,
-		uwp_intc_callback_t cb, void *arg);
+        uwp_intc_callback_t cb, void *arg);
 extern void uwp_aon_intc_unset_irq_callback(int channel);
+
+void aon_intc_uwp_init(void);
+void intc_uwp_init(void);
+void uwp_ictl_irq_enable(uwp_irq_type_t irq_type, unsigned int irq);
 
 #endif /* _INTC_UWP_H_ */
