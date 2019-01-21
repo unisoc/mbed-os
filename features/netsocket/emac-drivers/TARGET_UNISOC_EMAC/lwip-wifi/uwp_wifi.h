bb69e754a4d18938b79fb5d8c32d80f3e3ea445d
xiangkai.gao <xiangkai.gao@unisoc.com>
2019-01-21 19:11:01 +0800

    move wifi to feature

---
+++ features/netsocket/emac-drivers/TARGET_UNISOC_EMAC/lwip-wifi/uwp_wifi.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018 Texas Instruments, Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief General WiFi Definitions
+ */
+
+#ifndef MBED_UWP_WIFI_H
+#define MBED_UWP_WIFI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum wifi_security_type {
+	WIFI_SECURITY_TYPE_NONE = 0,
+	WIFI_SECURITY_TYPE_PSK,
+};
+
+#define NET_LINK_ADDR_MAX_LENGTH 6
+
+#define WIFI_SSID_MAX_LEN 32
+#define WIFI_PSK_MAX_LEN 64
+
+#define WIFI_CHANNEL_MAX 14
+#define WIFI_CHANNEL_ANY 255
+
+#define WIFI_BAND_2_4G	(1)
+#define WIFI_BAND_5G	(2)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_NET_WIFI_H_ */
